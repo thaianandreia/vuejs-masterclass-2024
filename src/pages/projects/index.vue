fbad932c8c45f9b6ac1c0ad955c78e0bb2da7886
thaianandreia <thaianandreia@gmail.com>
2024-11-01 15:47:45 -0300

    fetch projects

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref()
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) console.log(error)
+
+  projects.value = data
+  console.log('Projects: ', projects.value)
+})()
+</script>
 <template>
   <div>
     <h1>Projects</h1>
     <RouterLink to="/">Go to Home</RouterLink>
+    {{ projects }}
   </div>
 </template>
