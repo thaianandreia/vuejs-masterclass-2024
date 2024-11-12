bd3279a33d5f1cabb53bd2fae9c4f03bd71c1468
thaianandreia <thaianandreia@gmail.com>
2024-11-12 13:35:24 -0300

    buttons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import TopNavbar from '@/components/layout/TopNavbar.vue'
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
 import Sidebar from '@/components/Layout/Sidebar.vue'
 </script>
 
 <template>
-  <Sidebar></Sidebar>
+  <Sidebar />
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar></TopNavbar>
-    <main>
+    <TopNavbar />
+
+    <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
+      <div class="flex items-center">
+        <h1 class="text-lg font-semibold md:text-2xl">Page Title</h1>
+      </div>
       <RouterView />
     </main>
   </div>
