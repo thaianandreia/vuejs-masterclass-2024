3067b68d34874215c936e940b7f8c1a4e5db6b80
thaianandreia <thaianandreia@gmail.com>
2024-11-19 14:03:46 -0300

    auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -16,17 +17,28 @@ export default defineConfig({
   },
   plugins: [
     VueRouter(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
     vue({
-      template:{
-        compilerOptions:{
-          isCustomElement: element => element.startsWith('iconify-icon')
-        }
-      }
+      template: {
+        compilerOptions: {
+          isCustomElement: element => element.startsWith('iconify-icon'),
+        },
+      },
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
