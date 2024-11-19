118873e831da802263764615e02d974fe84d7c6f
thaianandreia <thaianandreia@gmail.com>
2024-11-19 15:55:18 -0300

    Suspense component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Component } from 'lucide-vue-next'
+</script>
 
 <template>
   <AuthLayout>
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+      <Suspense v-if="Component" :timeout="0">
+        <Component :is="Component" :key="route.name"></Component>
+        <template #fallback>
+          <span>Loading ..</span>
+        </template>
+      </Suspense>
+    </RouterView>
   </AuthLayout>
 </template>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -5,12 +5,15 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
 const projects = ref<Tables<'projects'>[] | null>(null)
-;(async () => {
+const getProjects = async () => {
   const { data, error } = await supabase.from('projects').select()
+
   if (error) console.log(error)
 
   projects.value = data
-})()
+}
+
+await getProjects()
 
 const columns: ColumnDef<Tables<'projects'>>[] = [
   {
