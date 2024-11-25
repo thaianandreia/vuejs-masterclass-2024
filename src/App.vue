5c2375b5cf8468926a6e00707e6333d7c3f868ef
thaianandreia <thaianandreia@gmail.com>
2024-11-25 17:15:16 -0300

    handlign js errors with hook

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-const { activeError } = storeToRefs(useErrorStore())
+const errorStore = useErrorStore()
+
+onErrorCaptured(error => {
+  errorStore.setError({
+    error: error,
+  })
+})
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError"></AppErrorPage>
+    <AppErrorPage v-if="errorStore.activeError"></AppErrorPage>
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name"></Component>
--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+// app.config.errorHandler = error => {}
+
 app.mount('#app')
